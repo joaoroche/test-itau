e6e6d7559402bc93915a8c701c79117b845028fe
João Roche <jcrlima@rdads.com.br>
2024-07-24 00:25:31 -0300

    feat: add routes for company and company details pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyComponent } from './pages/company/company.component';
+import { IdCompanyComponent } from './pages/company/id-company/id-company.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'company', pathMatch: 'full'
+  },
+  {
+    path: 'company', component: CompanyComponent,
+  },
+  {
+    path: 'company/:id', component: IdCompanyComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
