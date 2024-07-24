d21fff22c10039137295da84d1d293785e0a5628
João Roche <jcrlima@rdads.com.br>
2024-07-24 02:13:04 -0300

    feat: improve loading and error handling in id-company component

--- src/app/pages/company/id-company/id-company.component.ts
+++ src/app/pages/company/id-company/id-company.component.ts
@@ -3,13 +3,22 @@ import { ActivatedRoute } from '@angular/router';
 import { IBusiness } from 'src/app/models/company';
 import { CompanyService } from 'src/app/services/company.service';
 
+//TODO: Adicionar LOADING
+//TODO: Adicionar ERROR
+//TODO: Adicionar BOTÃO SALVAR
+
 @Component({
   selector: 'app-id-company',
   templateUrl: './id-company.component.html',
   styleUrls: ['./id-company.component.scss']
 })
 export class IdCompanyComponent implements OnInit {
+  // Data
   company!: IBusiness;
+
+  // Request
+  loading = false;
+  errorMessage = '';
 
  constructor(
     private route: ActivatedRoute,
@@ -27,14 +36,23 @@ export class IdCompanyComponent implements OnInit {
   }
 
   fetchCompany(id: number): void {
+    this.loading = true;
     this.companyService.fetchCompanyById(id).subscribe({
       next: (company: IBusiness) => {
         this.company = company;
+        this.loading = false;
+        this.errorMessage = '';
       },
       error: (error: any) => {
         console.error(error);
+        this.errorMessage = error;
+        this.loading = false;
       }
     });
+  }
+
+  back(): void {
+    window.history.back();
   }
 
 }
